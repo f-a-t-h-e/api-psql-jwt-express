320c7ce8e0909472f1b496de0991d9a9cabf46a0
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2022-10-30 23:58:58 +0200

    remove unusable type from db migrateions

--- migrations/sqls/20221027143943-tables-up.sql
+++ migrations/sqls/20221027143943-tables-up.sql
@@ -17,7 +17,6 @@ CREATE TABLE IF NOT EXISTS products (
 
 );
 DROP TYPE IF EXISTS STATUS;
-CREATE TYPE STATUS AS ENUM ('active', 'complete');
 CREATE TABLE IF NOT EXISTS orders (
         order_id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
         user_id uuid REFERENCES users(user_id) ON DELETE CASCADE,
