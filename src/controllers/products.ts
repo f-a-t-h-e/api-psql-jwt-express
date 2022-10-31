a328ba0553252b71db347deff4dd9317aa79f102
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2022-10-31 14:40:40 +0200

    Add try/catch

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -4,26 +4,43 @@ import Products from "../models/Product";
 const store = new Products();
 
 const getAll = async (_req: Request, res: Response): Promise<void> => {
+try{
   const data = await store.getAll();
   res.status(200).json({ data });
+}catch(err){
+  throw new Error(`Couldn't getAll products: ${err}`);
+}
 };
 const getByCat = async (req: Request, res: Response): Promise<void> => {
+try{
   const data = await store.getByCat(req.params.cat);
   res.status(200).json({ data });
+}catch(err){
+  throw new Error(`Couldn't getByCat products: ${err}`);
+}
 };
 const getTop = async (req: Request, res: Response): Promise<void> => {
+try{
   const data = await store.getTop(parseInt(req.params.num));
   res.status(200).json({ data });
+}catch(err){
+  throw new Error(`Couldn't getTop products: ${err}`);
+}
 };
 const getOne = async (req: Request, res: Response): Promise<void> => {
+try{
   const data = await store.getOne(req.params.id);
   res.status(200).json({ data });
+}catch(err){
+  throw new Error(`Couldn't getOne product: ${err}`);
+}
 };
 const create = async (
   req: Request,
   res: Response,
   _next: NextFunction
 ): Promise<void> => {
+try{
   const { name, price, catagory, user_id } = req.body;
   if (!name || !price || !catagory) {
     throw new Error("Please provide name, price and catagory.");
@@ -35,12 +52,16 @@ const create = async (
     catagory,
   });
   res.status(201).json({ data });
+}catch(err){
+  throw new Error(`Couldn't create product: ${err}`);
+}
 };
 const update = async (
   req: Request,
   res: Response,
   _next: NextFunction
 ): Promise<void> => {
+try{
   const { price, name, catagory, user_id } = req.body;
   const product_id = req.params.id;
   if (!name || !price || !catagory) {
@@ -54,14 +75,21 @@ const update = async (
     catagory,
   });
   res.status(200).json({ data });
+}catch(err){
+  throw new Error(`Couldn't update product: ${err}`);
+}
 };
 const Delete = async (
   req: Request,
   res: Response,
   _next: NextFunction
 ): Promise<void> => {
+try{
   const data = await store.delete(req.body.user_id, req.params.id);
   res.status(200).json({ data });
+}catch(err){
+  throw new Error(`Couldn't Delete product: ${err}`);
+}
 };
 
 export { getAll, getOne, create, update, Delete, getTop, getByCat };
